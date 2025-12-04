e83cf5762bd06186e618f3e65538aa5657aac89b
bletza47 <kedi.myjar@gmail.com>
2025-12-05 00:20:36 +0200

    Create 005_functions_find_max.py

---
+++ 005_functions_find_max.py
@@ -0,0 +1,11 @@
+# Write a function that accepts a list of numbers
+# and returns largest number
+
+
+def max_of_all(numbers):
+    return max(numbers)
+
+
+
+my_list = [2,3,5,34,62,63,77,85,90,94]
+print(max_of_all(my_list))
